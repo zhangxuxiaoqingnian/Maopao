e901c626078accc46e2347d25e66eff2c8f4ddde
小狼 <1131389540@qq.com>
2017-11-27 14:55:14 +0800

    你好

--- app/src/main/java/com/example/maopao/MainActivity.java
+++ app/src/main/java/com/example/maopao/MainActivity.java
@@ -11,7 +11,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        Integer[] arr={5,3,2,6,1,4};
+        for (int i = 0; i < arr.length; i++) {
+            int tem;
+            boolean flag=false;
+            for (int j = 0; j <arr.length-1; j++) {
 
-
+                if(arr[j]<arr[j+1]){
+                    tem=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=tem;
+                    flag=true;
+                }
+            }
+            if(!flag){
+                break;
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
     }
 }
